af8bb8820c4d72a9f5abe73701d5ddb9ccda517e
Phibaro.215 <baonico123@gmail.com>
2025-10-01 21:45:36 +0700

    Lưu thay đổi local _AdminLayout.cshtml

---
+++ CINEMA/Controllers/ComboController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CINEMA.Models;
+
+namespace CINEMA.Controllers
+{
+    public class ComboController : Controller
+    {
+        private readonly CinemaContext _context;
+
+        public ComboController(CinemaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Combo
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Combos.ToListAsync());
+        }
+
+        // GET: Combo/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var combo = await _context.Combos
+                .FirstOrDefaultAsync(m => m.ComboId == id);
+            if (combo == null)
+            {
+                return NotFound();
+            }
+
+            return View(combo);
+        }
+
+        // GET: Combo/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Combo/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ComboId,Name,Description,Price,ImageUrl,IsActive")] Combo combo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(combo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(combo);
+        }
+
+        // GET: Combo/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var combo = await _context.Combos.FindAsync(id);
+            if (combo == null)
+            {
+                return NotFound();
+            }
+            return View(combo);
+        }
+
+        // POST: Combo/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ComboId,Name,Description,Price,ImageUrl,IsActive")] Combo combo)
+        {
+            if (id != combo.ComboId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(combo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ComboExists(combo.ComboId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(combo);
+        }
+
+        // GET: Combo/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var combo = await _context.Combos
+                .FirstOrDefaultAsync(m => m.ComboId == id);
+            if (combo == null)
+            {
+                return NotFound();
+            }
+
+            return View(combo);
+        }
+
+        // POST: Combo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var combo = await _context.Combos.FindAsync(id);
+            if (combo != null)
+            {
+                _context.Combos.Remove(combo);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ComboExists(int id)
+        {
+            return _context.Combos.Any(e => e.ComboId == id);
+        }
+    }
+}
